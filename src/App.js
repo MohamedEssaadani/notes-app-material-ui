13c92e6d0877d5d4ead99c96ade5d238c590bd9a
Mohamed Es-saadani <essaadani80@gmail.com>
2022-03-07 18:01:53 +0100

    add note form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateNote from "./pages/CreateNote";
 import Notes from "./pages/Notes";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+// create custom theme, (override the theme)
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Notes />} />
-        <Route path="/" element={<CreateNote />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create-note" element={<CreateNote />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,5 +1,92 @@
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
 const CreateNote = () => {
-  return <div>Create Note</div>;
+  // title & details for the form
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  // for title & details errors
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  // handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // reset errors
+    setTitleError(true);
+    setTitleError(true);
+
+    // if title is empty, set title error to true
+    if (title === "") {
+      setTitleError(true);
+    }
+
+    // if details is empty, set details error to true
+    if (details === "") {
+      setDetailsError(true);
+    }
+
+    //check for title & details and then add the note
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
+  return (
+    <Container size="sm">
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="false" onSubmit={handleSubmit}>
+        <TextField
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          sx={{ display: "block", marginTop: 5, marginBottom: 5 }}
+          fullWidth
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          error={titleError}
+        />
+
+        <TextField
+          label="Détails"
+          variant="outlined"
+          color="secondary"
+          sx={{ display: "block", marginTop: 5, marginBottom: 5 }}
+          multiline
+          rows={4}
+          fullWidth
+          required
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+          error={detailsError}
+        />
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<ArrowForwardIosIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
 };
 
 export default CreateNote;
